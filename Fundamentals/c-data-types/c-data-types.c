2ad1b19bcef32a0e8746c4d82279b2f3b6e4d8fb
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-04 22:33:26 +0200

    2nd commit

---
+++ Fundamentals/c-data-types/c-data-types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    // check sizes of the above variables
+    printf("Size of short = %d bytes\n", sizeof(a));
+    printf("Size of long = %d bytes\n", sizeof(b));
+    printf("Size of long long = %d bytes\n", sizeof(c));
+    printf("Size of long double = %d bytes\n", sizeof(d));
+
+    return 0;
+}
